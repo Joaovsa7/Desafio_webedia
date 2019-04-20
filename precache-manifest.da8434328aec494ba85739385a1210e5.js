e11c7a877a87cb5d23b57e6f3676f66594439616
joao vitor <jvsadesign@gmail.com>
2019-04-20 15:14:28 -0300

    Updates

---
+++ precache-manifest.da8434328aec494ba85739385a1210e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec3a525b003c1089c155",
+    "url": "/desafio_webedia/static/js/runtime~main.f950e125.js"
+  },
+  {
+    "revision": "66f14b55473a79e459f4",
+    "url": "/desafio_webedia/static/js/main.33132998.chunk.js"
+  },
+  {
+    "revision": "1af5dafddc7d14f1eaff",
+    "url": "/desafio_webedia/static/js/2.90336fb5.chunk.js"
+  },
+  {
+    "revision": "66f14b55473a79e459f4",
+    "url": "/desafio_webedia/static/css/main.48b86491.chunk.css"
+  },
+  {
+    "revision": "ddc56bded11b45297cedb6b482896e68",
+    "url": "/desafio_webedia/index.html"
+  }
+];
